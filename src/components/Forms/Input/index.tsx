756b6a959878984ad618617ce56e55322b04d83a
hitalo <hitalo.2014.hs@gmail.com>
2023-02-16 11:11:52 -0300

    feat: header made + pre setup for input component

---
+++ src/components/Forms/Input/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+function Input() {
+  return (
+    <View>
+      <Text>Input</Text>
+    </View>
+  );
+}
+
+export default Input;
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,11 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { Container } from "./styles";
+import { Container, Header, Title } from "./styles";
 
 function Register() {
   return (
     <Container>
-      <Text>Register page</Text>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
     </Container>
   );
 }
