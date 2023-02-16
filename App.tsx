0476e334dfde5ac58ea6cfb5ed3703a521e0c94b
hitalo <hitalo.2014.hs@gmail.com>
2023-02-16 10:58:50 -0300

    feat: pre setup for register page

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ import * as Font from "expo-font";
 
 import Dashboard from "./src/screens/Dashboard";
 import theme from "./global/styles/theme";
-import HighlightCard from "./src/components/HighlightCard";
+import Register from "./src/screens/Register";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -52,7 +52,8 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      {/* <Dashboard /> */}
+      <Register />
     </ThemeProvider>
   );
 }
---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Container } from "./styles";
+
+function Register() {
+  return (
+    <Container>
+      <Text>Register page</Text>
+    </Container>
+  );
+}
+
+export default Register;
