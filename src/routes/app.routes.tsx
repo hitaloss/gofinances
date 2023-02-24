0c061ba5e953f803804a127ed83dcf76f5cd21e3
hitalo <hitalo.2014.hs@gmail.com>
2023-02-24 18:09:58 -0300

    feat: created and modified tab router

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+import { useTheme } from "styled-components";
+
 import Dashboard from "../screens/Dashboard";
 import Register from "../screens/Register";
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 function AppRoutes() {
+  const theme = useTheme();
+
   return (
-    <Navigator>
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.secondary,
+        tabBarInactiveTintColor: theme.colors.captions,
+      }}
+    >
       <Screen name="Listagem" component={Dashboard} />
       <Screen name="Cadastrar" component={Register} />
+      <Screen name="Resumo" component={Register} />
     </Navigator>
   );
 }
