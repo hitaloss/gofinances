5a66f38d62191dcf393f7f27072a9ac58436b396
hitalo <hitalo.2014.hs@gmail.com>
2023-03-16 09:30:32 -0300

    feat: added base setup for resume screen

---
+++ src/screens/Resume/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Title } from "./styles";
+
+function Resume() {
+  return (
+    <Container>
+      <Title>Resumo por categoria</Title>
+    </Container>
+  );
+}
+
+export default Resume;
