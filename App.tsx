b91a5e98bacfb178866affd816c7d0cd2fc402d0
hitalo <hitalo.2014.hs@gmail.com>
2023-03-20 16:10:31 -0300

    feat: added light barStyle setting for the application

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import AppRoutes from "./src/routes/app.routes";
 
@@ -55,6 +56,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
+        <StatusBar barStyle="light-content" />
         <AppRoutes />
       </NavigationContainer>
     </ThemeProvider>
