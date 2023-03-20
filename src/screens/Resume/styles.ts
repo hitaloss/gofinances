7dd027d075c96aa45d7f0405a3c984863ed0b4e6
hitalo <hitalo.2014.hs@gmail.com>
2023-03-20 16:20:46 -0300

    style: added custom components for resume page using styled-components

--- src/screens/Resume/styles.ts
+++ src/screens/Resume/styles.ts
@@ -1,5 +1,7 @@
-import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
+import { BorderlessButton } from "react-native-gesture-handler";
+import { RFValue } from "react-native-responsive-fontsize";
+import { Feather } from "@expo/vector-icons"
 
 export const Container = styled.View`
     flex: 1;
@@ -22,9 +24,38 @@ export const Title = styled.Text`
     text-transform: capitalize;
 `;
 
-export const Content = styled.ScrollView.attrs({
-    contentContainerStyle: {
-        flex: 1, 
-        padding: 24,
-    }}
-)``
+export const Content = styled.ScrollView``;
+
+export const ChartContainer = styled.View`
+    width: 100%;
+    align-items: center;
+`;
+
+export const MonthSelector = styled.View`
+    width: 100%;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-top: 24px;
+`;
+
+export const Month = styled.Text`
+    font-family: ${({theme}) => theme.fonts.regular};
+    font-size: ${RFValue(20)}px;
+    text-transform: capitalize;
+`;
+
+export const MonthAction = styled(BorderlessButton)`
+    font-size: ${RFValue(24)}px;
+`;
+
+export const Icon = styled(Feather)`
+    font-size: ${RFValue(24)}px;
+`;
+
+export const LoadContainer = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
