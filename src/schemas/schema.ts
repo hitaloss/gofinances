2e923ee97d78b1a5a1e26627fe9109f3a2fe6851
hitalo <hitalo.2014.hs@gmail.com>
2023-03-20 16:11:59 -0300

    fix: added required setting for amount field on register schema

--- src/schemas/schema.ts
+++ src/schemas/schema.ts
@@ -4,6 +4,7 @@ export const categorySchema = yup.object().shape({
     name: yup.string().required("Nome é obrigatório"),
     amount: yup
     .number()
+    .required("Preço é obrigatório")
     .typeError("Informe um valor numérico")
     .positive("O valor deve ser positivo")
 })
