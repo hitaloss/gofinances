d4b91551e4b7c91dab7565a6357de654802261c9
hitalo <hitalo.2014.hs@gmail.com>
2023-03-24 16:43:04 -0300

    refactor: turn TouchableOpacity into RectButton, also fixed category buttons, allowing to click just once

--- src/components/Forms/SelectCategory/styles.ts
+++ src/components/Forms/SelectCategory/styles.ts
@@ -1,16 +1,14 @@
 import styled from "styled-components/native";
 import { Feather } from "@expo/vector-icons"
 import { RFValue } from "react-native-responsive-fontsize";
+import { RectButton } from "react-native-gesture-handler";
 
 
-export const Container = styled.TouchableOpacity.attrs({
-    activeOpacity: 0.7
-})`
+export const Container = styled(RectButton)`
     background-color: ${({theme}) => theme.colors.shape};
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-
     border-radius: 5px;
     padding: 18px 16px;
     margin-top: 16px;
