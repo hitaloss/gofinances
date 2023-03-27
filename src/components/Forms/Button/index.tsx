9eb60bc14b6a18ab7b445137f6adca1526534ad6
hitalo <hitalo.2014.hs@gmail.com>
2023-03-27 09:39:51 -0300

    refactor: removed comments and unused imports

--- src/components/Forms/Button/index.tsx
+++ src/components/Forms/Button/index.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import { Container, Title } from "./styles";
-import { TouchableOpacityProps } from "react-native";
 import { RectButtonProps } from "react-native-gesture-handler";
-
-// interface Props extends TouchableOpacityProps {
-//   text: string;
-// }
 
 interface Props extends RectButtonProps {
   text: string;
