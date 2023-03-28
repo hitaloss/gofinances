b45575a52eb0c81c0acc16f48742e3cdbd58d8ef
hitalo <hitalo.2014.hs@gmail.com>
2023-03-28 12:49:40 -0300

    refactor: removed any unused imports

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { StatusBar } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import AppRoutes from "./src/routes/app.routes";
 
 import { ThemeProvider } from "styled-components";
 import {
@@ -16,7 +14,6 @@ import * as Font from "expo-font";
 
 import theme from "./global/styles/theme";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import SignIn from "./src/screens/SignIn";
 import Providers from "./src/contexts";
 import Routes from "./src/routes";
 
